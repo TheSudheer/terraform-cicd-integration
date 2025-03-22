e5cc196cd0bc87a3947659f2b8ac08d9f35da49e
sudheer <sudheersunny129@gmail.com>
2025-03-22 19:41:42 +0530

    Accept the image name as the first parameter from Jenkinsfile

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # This script runs on the EC2 instance to deploy my Docker Compose setup
 
+# Accept the image name as the first parameter
+export IMAGE=$1
+
 # Start Docker Compose in detached mode using the docker-compose.yaml file
 docker-compose -f docker-compose.yml up --detach
 
