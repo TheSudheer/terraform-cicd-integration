0dbe16268009ed705133eec509b7717c72bcdc0a
sudheer <sudheersunny129@gmail.com>
2025-03-22 19:15:14 +0530

    fix: .yaml to .yml

--- server-cmds.sh
+++ server-cmds.sh
@@ -2,7 +2,7 @@
 # This script runs on the EC2 instance to deploy my Docker Compose setup
 
 # Start Docker Compose in detached mode using the docker-compose.yaml file
-docker-compose -f docker-compose.yaml up --detach
+docker-compose -f docker-compose.yml up --detach
 
 # Print a success message
 echo "Deployment successful!"
