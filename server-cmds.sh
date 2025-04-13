a59948b7ddcdf5ccfd19cc0821c6982c53ea76fa
sudheer <sudheersunny129@gmail.com>
2025-04-13 16:08:45 +0530

    feat: Add Docker and Docker Compose installation script
    - Updates the system and installs Docker.
    - Starts and enables the Docker service.
    - Sets proper permissions on the Docker socket.
    - Adds the current user to the docker group.
    - Pulls and runs an NGINX container in detached mode.
    - Downloads and installs Docker Compose.

--- server-cmds.sh
+++ server-cmds.sh
@@ -3,10 +3,15 @@
 
 # Accept the image name as the first parameter
 export IMAGE=$1
+export DOCKER_USER=$2
+export DOCKER_PWD=$3
+
+echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
 
 # Start Docker Compose in detached mode using the docker-compose.yaml file
 docker-compose -f docker-compose.yml up --detach
 
 # Print a success message
 echo "Deployment successful!"
+
 
